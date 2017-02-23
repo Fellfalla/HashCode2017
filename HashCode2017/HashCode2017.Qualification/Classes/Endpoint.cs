637eacde81406a07bd74faf214f5fd6f910cca6c
joop0 <joop0@EASYNOTETJ75>
2017-02-23 19:09:26 +0100

    class test

---
+++ HashCode2017/HashCode2017.Qualification/Classes/Endpoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2017.Qualification.Classes
+{
+    class Endpoint
+    {
+        private int _latencyToDataCenter;
+
+        private List<CacheConnection> _cacheConnections;
+
+        public Endpoint(int latencyToDataCenter, List<CacheConnection> cacheConnections)
+        {
+            _latencyToDataCenter = latencyToDataCenter;
+            _cacheConnections = cacheConnections;
+        }
+
+        public struct CacheConnection
+        {
+            public int id;
+            public int latency;
+        }
+
+
+    }
+}
---
+++ HashCode2017/HashCode2017.Qualification/Classes/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2017.Qualification.Classes
+{
+    class Video
+    {
+        private uint _size;
+
+        public Video(uint size)
+        {
+            _size = size;
+        }
+    }
+}
