750ca9aa15ea37ab98615c0807fbe9a5b2287294
joop0 <joop0@EASYNOTETJ75>
2017-02-23 20:52:44 +0100

    costlist

--- HashCode2017/HashCode2017.Qualification/Classes/GreedyAssign.cs
+++ HashCode2017/HashCode2017.Qualification/Classes/GreedyAssign.cs
@@ -8,13 +8,21 @@ namespace HashCode2017.Qualification.Classes
 {
     public class GreedyAssign
     {
-
+        public List<Item> CostList = new List<Item>();
 
         public struct Item
         {
             public Video Video;
             public CacheServer Cache;
             public float HeuristicCosts;
+        }
+
+        public void InsertToList(Item item)
+        {
+            foreach (Item element in CostList)
+            {
+                
+            }
         }
 
     }
