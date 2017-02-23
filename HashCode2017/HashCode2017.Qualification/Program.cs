db2f87f2a7508da5d965c4efefab849045ddcba4
joop0 <joop0@EASYNOTETJ75>
2017-02-23 19:47:29 +0100

    program basic console

--- HashCode2017/HashCode2017.Qualification/Program.cs
+++ HashCode2017/HashCode2017.Qualification/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HashCode2017.Practice;
+using HashCode2017.Qualification.Classes;
 
 namespace HashCode2017.Qualification
 {
@@ -10,6 +12,24 @@ namespace HashCode2017.Qualification
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input int 0-4 for requested input");
+            Console.WriteLine("0 = kittens");
+            Console.WriteLine("1 = me_at_the_zoo");
+            Console.WriteLine("2 = trending_today");
+            Console.WriteLine("3 = videos_worth_spreading");
+            String s;
+             s = Console.ReadLine();
+            Console.WriteLine("Input " + s);
+
+            //Wir gehen davon aus dass wir nur die richtigen Zahlen eingeben.
+            int input = int.Parse(s);
+
+            List<Video> videos;
+            List<Endpoint> endpoints;
+            
+            DataParser.ParseFileLines(DataParser.ReadFile((DataParser.ProblemSettings) input), out videos, out endpoints);
+
+
 
         }
     }
