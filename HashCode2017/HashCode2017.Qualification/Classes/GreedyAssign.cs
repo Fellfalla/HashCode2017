43fec1998e720c5d5d82b43dd6759a80091b1221
joop0 <joop0@EASYNOTETJ75>
2017-02-23 20:46:30 +0100

    greedy assign

---
+++ HashCode2017/HashCode2017.Qualification/Classes/GreedyAssign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2017.Qualification.Classes
+{
+    public class GreedyAssign
+    {
+
+        public struct Item
+        {
+            public Video Video;
+            public CacheServer Cache;
+            public float HeuristicCosts;
+        }
+
+    }
+}
