88860f3903fe70ba01c48585ab302dd743f99420
joop0 <joop0@EASYNOTETJ75>
2017-02-23 21:21:33 +0100

    writeoutput

--- HashCode2017/HashCode2017.Qualification/Program.cs
+++ HashCode2017/HashCode2017.Qualification/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,26 +22,30 @@ namespace HashCode2017.Qualification
              s = Console.ReadLine();
             Console.WriteLine("Input " + s);
 
+            string[] output;
+
             //Wir gehen davon aus dass wir nur die richtigen Zahlen eingeben.
             int input;
             if (int.TryParse(s, out  input))
             {   
                 // user has type 0 1 2 or 3
-                EvaluateData((DataParser.ProblemSettings) input);
+                output = EvaluateData((DataParser.ProblemSettings) input);
+                WriteOutput(((DataParser.ProblemSettings) input).ToString(), output);
             }
             else
             {
                 foreach (var mode in Enum.GetNames(typeof(DataParser.ProblemSettings)))
                 {
                     var modeEnum = (DataParser.ProblemSettings) Enum.Parse(typeof(DataParser.ProblemSettings), mode);
-                    EvaluateData(modeEnum);
+                    output = EvaluateData(modeEnum);
+                     WriteOutput(mode, output);
                 }
                 // solve all input files
 
             }
         }
 
-        public static void EvaluateData(DataParser.ProblemSettings mode)
+        public static string[] EvaluateData(DataParser.ProblemSettings mode)
         {
             Video[] videos;
             Endpoint[] endpoints;
@@ -62,12 +67,43 @@ namespace HashCode2017.Qualification
 
                 //2 while still space on any cache server: find biggest entry in M and assign V to C --> then set enty to 0. As soon as any cacheserver is full: delete column in M
 
-
-
-
+            return GetOutput(cacheServers);
         }
 
-        
+        private static string[] GetOutput(CacheServer[] cacheServers)
+        {
+            
+            string[] output = new string[cacheServers.Length 
+                 + 1];
+
+            output[0] = cacheServers.Length.ToString();
+
+            for (int index = 0; index < cacheServers.Length; index++)
+            {
+                CacheServer cache = cacheServers[index];
+
+                int stringindex = index + 1;
+
+                output[stringindex] = cache.Id.ToString();
+
+                //Now all the videos stored
+                foreach (Video video in cache.Videos)
+                {
+                    output[stringindex] += " " + video.Id;
+                }
+            }
+
+            return output;
+        }
+
+        private static void WriteOutput(string filename, string[] output)
+        {
+            var outputDir = Directory.GetCurrentDirectory();
+            var file = Path.Combine(outputDir, filename + ".out");
+                File.WriteAllLines(file, output);
+        }
+
+
         private static void ProgressHandler(float f)
         {
             Console.Write("\rProgress: {0} %\t\t", f * 100);
