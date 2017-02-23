66588dedbc3662b1c664a0b43418312668fdf69c
hschmidtchen <hagen.schmidtchen@gmail.com>
2017-02-23 20:28:09 +0100

    AssignerClass

---
+++ HashCode2017/HashCode2017.Qualification/Classes/Assigner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2017.Qualification.Classes
+{
+    class Assigner
+    {
+        public static void Assign()
+        {
+            foreach (var item in GreedyAssign.CostList)
+            {
+                item.Cache.PutVideo(item.Video);
+            }
+
+        }
+    }
+}
